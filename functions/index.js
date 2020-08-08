7154315450bc0d9e327e9ef2e80033ed8f86385f
MP <ping.mithil@gmail.com>
2020-08-08 10:19:11 -0700

    .

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const express = require('express');
+var favicon = require('serve-favicon')
+const path = require('path');
 
 const app = express();
 
@@ -7,7 +9,13 @@ app.set('views','./views')
 
 app.set('view engine', 'ejs');
 
-app
+app.use(favicon(path.join(__dirname, '/', 'favicon.ico')))
+
+app.get('/',function (req,res) {
+  //cached for a month
+  res.setHeader("Cache-Control", "public, max-age=2592000");
+  res.render("index")
+})
 
 app.use(function (req, res, next) {
   res.status(404).render('404')
