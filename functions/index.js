b12db9e77c3d699245da419260a986fcdf8d8c98
MP <ping.mithil@gmail.com>
2020-08-07 20:48:59 -0700

    404 Page

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.set('views','./views')
+
+app.set('view engine', 'ejs');
+
+app
+
+app.use(function (req, res, next) {
+  res.status(404).render('404')
+})
+
+exports.app = functions.https.onRequest(app);
