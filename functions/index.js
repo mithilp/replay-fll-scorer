cae86b2cd9cf3eba4459b3f4f4cc2900e3195455
MP <ping.mithil@gmail.com>
2021-01-17 10:47:07 -0800

    Update index.js
    Updated how sitemaps.xml and robots.txt work

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,38 @@
-const functions = require('firebase-functions');
-const express = require('express');
-var favicon = require('serve-favicon');
-const path = require('path');
-var sitemap = require('express-sitemap');
+const functions = require("firebase-functions");
+const express = require("express");
+var favicon = require("serve-favicon");
+const path = require("path");
 
 const app = express();
 
-app.set('views','./views')
+app.set("views", "./views");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(favicon(path.join(__dirname, '/', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, "/", "favicon.ico")));
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use("/static", express.static(__dirname + "/public"));
 
-app.get('/',function (req,res) {
-  //cached for a month
-  res.setHeader("Cache-Control", "public, max-age=2592000");
-  res.render("index")
-})
-
-var map = sitemap({
-    generate: app
+app.get("/", function (req, res) {
+	//cached for a month
+	res.setHeader("Cache-Control", "public, max-age=2592000");
+	res.render("index");
 });
 
-app.get('/sitemap.xml', function(req, res) { // send XML map
-    map.XMLtoWeb(res);
-}).get('/robots.txt', function(req, res) { // send TXT map
-    map.TXTtoWeb(res);
-});
+app
+	.get("/sitemap.xml", function (req, res) {
+		// send XML map
+		res.sendFile(__dirname + "/sitemap.xml");
+	})
+	.get("/robots.txt", function (req, res) {
+		// send TXT map
+		res.sendFile(__dirname + "/robots.txt");
+	});
 
 app.use(function (req, res, next) {
-  res.status(404).send("Error 404. The URL you are trying to go to doesn't exist.")
-})
+	res
+		.status(404)
+		.send("Error 404. The URL you are trying to go to doesn't exist.");
+});
 
 exports.app = functions.https.onRequest(app);
